1fa639db9a5abe43122c4e4cc37c5f6dbb3f9be6
frendyUAJY <220711608@students.uajy.ac.id>
2025-04-07 14:40:46 +0700

    menambahkan organisasi seed

---
+++ prisma/seeding/organisasi.seed.js
@@ -0,0 +1,93 @@
+import { prismaClient } from "../../src/application/database.js";
+import { logger } from "../../src/application/logging.js";
+
+const listOrganisasi = [
+  {
+    id_organisasi: "ORG1",
+    nama_organisasi: "Yayasan Dharma Bakti",
+    alamat: "Jl. Babarsari, No. 14",
+    nomor_telepon: "08123123123",
+    deskripsi: "Yayasan penyaluran bantuan bencana",
+  },
+  {
+    id_organisasi: "ORG2",
+    nama_organisasi: "Lembaga Cinta Kasih",
+    alamat: "Jl. Kaliurang KM 7",
+    nomor_telepon: "08123456789",
+    deskripsi: "Lembaga sosial peduli anak yatim",
+  },
+  {
+    id_organisasi: "ORG3",
+    nama_organisasi: "Forum Hijau Indonesia",
+    alamat: "Jl. Gejayan No. 5",
+    nomor_telepon: "082112223333",
+    deskripsi: "Organisasi pelestarian lingkungan",
+  },
+  {
+    id_organisasi: "ORG4",
+    nama_organisasi: "Komunitas Peduli Pendidikan",
+    alamat: "Jl. Magelang KM 4",
+    nomor_telepon: "085266778899",
+    deskripsi: "Komunitas relawan pendidikan anak",
+  },
+  {
+    id_organisasi: "ORG5",
+    nama_organisasi: "Bakti Sosial Nusantara",
+    alamat: "Jl. Solo No. 10",
+    nomor_telepon: "08199887766",
+    deskripsi: "Organisasi bantuan sosial dan medis",
+  },
+  {
+    id_organisasi: "ORG6",
+    nama_organisasi: "Relawan Tanggap Darurat",
+    alamat: "Jl. Parangtritis KM 3",
+    nomor_telepon: "089611223344",
+    deskripsi: "Tim relawan untuk tanggap bencana",
+  },
+  {
+    id_organisasi: "ORG7",
+    nama_organisasi: "Gerakan Donor Darah",
+    alamat: "Jl. Ringroad Utara No. 8",
+    nomor_telepon: "087733221100",
+    deskripsi: "Komunitas penggerak donor darah",
+  },
+  {
+    id_organisasi: "ORG8",
+    nama_organisasi: "Sahabat Lansia",
+    alamat: "Jl. Palagan Tentara Pelajar",
+    nomor_telepon: "08122334455",
+    deskripsi: "Layanan sosial untuk lansia",
+  },
+  {
+    id_organisasi: "ORG9",
+    nama_organisasi: "Rumah Harapan",
+    alamat: "Jl. Seturan No. 15",
+    nomor_telepon: "083144556677",
+    deskripsi: "Pusat rehabilitasi dan konseling",
+  },
+  {
+    id_organisasi: "ORG10",
+    nama_organisasi: "Pemuda Berkarya",
+    alamat: "Jl. Affandi No. 23",
+    nomor_telepon: "082345678901",
+    deskripsi: "Organisasi pengembangan pemuda",
+  }
+];
+
+export async function organisasiSeeding() {
+  await prismaClient.organisasi.createMany({
+    data: listOrganisasi,
+  });
+}
+
+organisasiSeeding()
+  .then(() => {
+    logger.info("Berhasil menambahkan seluruh seeding organisasi");
+  })
+  .catch((e) => {
+    logger.info(e);
+    process.exit(1);
+  })
+  .finally(() => {
+    prismaClient.$disconnect();
+  });
