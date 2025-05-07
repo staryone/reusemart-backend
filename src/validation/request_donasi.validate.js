7ce93646e20b7dc51d98ba8cf9d41f9655427914
StaryOne <stanyslausharyone@gmail.com>
2025-05-07 23:10:47 +0700

    menambahkan request donasi validate dan file request donasi service

--- src/validation/request_donasi.validate.js
+++ src/validation/request_donasi.validate.js
@@ -1,17 +1,12 @@
 import Joi from "joi";
 
 const createRequestDonasiValidation = Joi.object({
-  nama_request_donasi: Joi.string().max(255).required(),
-  detail_request_donasi: Joi.string().max(255).required(),
+  deskripsi: Joi.string().max(255).required(),
   id_organisasi: Joi.number().required(),
 });
 const getRequestDonasiValidation = Joi.number().required();
 const updateRequestDonasiValidation = Joi.object({
-  id_request_donasi: Joi.number().required(),
-  id_pembeli: Joi.number().required(),
-  nama_request_donasi: Joi.string().max(255).optional(),
-  detail_request_donasi: Joi.string().max(255).optional(),
-  status_default: Joi.boolean().optional(),
+  deskripsi: Joi.string().max(255).required(),
 });
 
 export {
